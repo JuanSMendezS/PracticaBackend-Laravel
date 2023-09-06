e1e22404353d263cb710c861e283132781329094
JuanMendez <mendezsamperjuan@gmail.com>
2023-09-05 22:15:48 -0500

    Prueba de commit inicial

---
+++ database/migrations/2023_09_06_014839_create_avisos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('avisos', function (Blueprint $table) {
+            $table->string('code', 50)->unique('CODE')->comment('Ref cod');
+            $table->string('title');
+            // Lugar de la llave foránea code_area
+            $table->timestamps();
+            $table->timestamp('date_expiration');
+            $table->timestamp('date_published');
+            $table->string('title');
+            $table->tinyInteger('state'); // expires - draft - erased - published
+            $table->string('url_document')->unique()->comment('Link to Document');
+            // Llave llave foránea code_user
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('avisos');
+    }
+};
